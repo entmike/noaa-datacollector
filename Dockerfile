3058bb6b1cecfb452f33103816c17832dcc0a432
Mike Howles <mike.howles@gmail.com>
2020-12-07 13:11:01 -0600

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:15.2.1-alpine3.10
+
+RUN mkdir -p /usr/src
+WORKDIR /usr/src
+
+RUN apk update && apk upgrade
+RUN apk add git
+
+ARG CACHE_DATE=1900-01-01
+RUN git clone https://github.com/entmike/noaa-datacollector.git
+
+WORKDIR /usr/src/hubitat-datacollector
+RUN npm i
+
+ENV INTERVAL=3600
+
+CMD ["npm","start"]
---
+++ index.js
@@ -0,0 +1,102 @@
+require('dotenv').config()
+
+const { Client } = require('pg')
+const axios = require('axios')
+
+const LATITUDE = process.env.LATITUDE || 40.70098574912939
+const LONGITUDE = process.env.LONGITUDE || -73.9837906003035
+const INTERVAL = process.env.INTERVAL || 1800
+let url
+
+console.log(`Getting Forecast URL for lat/lng: ${LATITUDE},${LONGITUDE}...`)
+const pollNOAA = () => {
+  axios
+    .get(url)
+    .then((response) => {
+      if (
+        response &&
+        response.data &&
+        response.data.properties &&
+        response.data.properties.periods
+      ) {
+        const periods = response.data.properties.periods
+        return periods
+      }
+    })
+    .then((data) => {
+      const header = []
+      const AoA = []
+      data.forEach((obj) => {
+        Object.keys(obj).forEach(
+          (key) => header.includes(key) || header.push(key)
+        )
+        const thisRow = new Array(header.length)
+        header.forEach((col, i) => (thisRow[i] = obj[col] || ''))
+        AoA.push(thisRow)
+      })
+      // AoA.unshift(header)
+      const client = new Client()
+      client.connect().then(() => {
+        // console.log(data)
+        const a = []
+        data.forEach((obj) => {
+          a.push(`(
+              ${obj.number},
+              '${obj.name}',
+              '${obj.startTime}',
+              '${obj.endTime}',
+              ${obj.isDaytime === undefined ? false : obj.isDaytime},
+              ${obj.temperature},
+              '${obj.temperatureUnit}',
+              '${obj.temperatureTrend}',
+              '${obj.windSpeed}',
+              '${obj.windDirection}',
+              '${obj.icon}',
+              '${obj.shortForecast}',
+              '${obj.detailedForecast}'
+            )`)
+        })
+        const s = a.join(', ')
+        const query = `DELETE FROM forecast;
+          INSERT INTO forecast 
+              (number, name, "startTime", "endTime", "isDaytime", temperature, "temperatureUnit", "temperatureTrend", "windSpeed", "windDirection",
+              icon, "shortForecast", "detailedForecast")
+              VALUES ${s}` // Replace double quotes with singular ones.}`,
+
+        client
+          .query(query)
+          .then((r) => {
+            // console.log(r)
+            client.end()
+          })
+          .catch((e) => {
+            console.log(e)
+            client.end()
+          })
+      })
+    })
+
+  console.log(url)
+}
+// Make a request for a user with a given ID
+axios
+  .get(`https://api.weather.gov/points/${LATITUDE},${LONGITUDE}`)
+  .then((response) => {
+    // handle success
+    if (
+      response &&
+      response.data &&
+      response.data.properties &&
+      response.data.properties.forecast
+    ) {
+      url = response.data.properties.forecast
+      console.log(`Forecast URL: ${url}`)
+    }
+    pollNOAA()
+    console.log(`Querying ${url} every ${INTERVAL} seconds...`)
+    setInterval(pollNOAA, INTERVAL * 1000)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error)
+  })
